c64f28ab8de902fc930b3c2123ba0932a5251ce5
Guhirwa <guhirwachristian004@gmail.com>
2025-10-19 22:11:41 +0200

    Revisiting the content of filesystem, modules, os, stream and buffer

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((request, responce ) => {
-    console.log(request.url, request.method);
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log("listening for request on port 3000")
-}); 
