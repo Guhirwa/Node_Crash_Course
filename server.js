7d2618aaca005790f9de0887ebb4da8abf8ff395
Guhirwa <guhirwachristian004@gmail.com>
2025-10-19 23:19:28 +0200

    chore: request object
    - request object wraps many kind of data that are very useful
    - request.url and request.method can help us to get the url user used and the methode of the request vice versa.
    - request.url is very useful in routing and other concepts because can be used to determined what to send back as a responce to the user request.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) =>{
-    console.log('Request made');
+const server = http.createServer((request, responce) => {
+    console.log(request.url, request.method);
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Listening for request on port 3000')
-})
+server.listen(3002, 'localhost', () => {
+    console.log('listening for request on port 3002');
+});
