5207750b0c179689fecdff1eb2e99b43c0078b89
Guhirwa <christianguhirwa74@gmail.com>
2025-10-22 17:19:15 +0200

    feat(app): create view / template engine
    - used for creating dynamic html files for responces
    - having view directory containg those html-like files with an extention of .ejs is mandatory for default lookup what to render there; otherwise configure the lookup meckanism to the other directory
    - create the .ejs file tobe rendered according to the request received
    - create 404 using .use as a middleware to handle the invalid request url

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ const express = require('express');
 // Express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+// in case view folder have another name like `myviews`
+// app.set('views', 'myviews');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (request, responce) => {
-    responce.sendFile('./views/index.html', { root: __dirname });
-})
+    responce.render('index');
+});
 
 app.get('/about', (request, responce) => {
-    responce.sendFile('./views/about.html', { root: __dirname });
-})
+    responce.render('about');
+});
+
+app.get('/blogs/create', (request, responce) => {
+    responce.render('create');
+});
 
 // redirects
 app.get('/about-me', (request, responce) => {
     responce.redirect('/about');
-})
+});
 
 app.get('/about-us', (request,responce) => {
     responce.redirect('/about');
-})
+});
 
 // 404 page
 app.use((request, responce) => {
-    responce.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+    responce.status(404).render('404');
+});
