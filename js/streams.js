39721c4459f8d7ee940979171b034c306c719662
Guhirwa <christianguhirwa74@gmail.com>
2025-11-13 17:33:32 +0200

    chore(App): refactoring some files
    - after creating and intergrating my project with react app, package.json got rewritted (loss all third party packages installed)
    - install nodemon: package to monitor node and rerun the server after every update
    - install lodash: comes with different precious methods
    - install ejs: helps to add the dynamic html-like files
    - install express: for simplifying and reduce nodeJs boilerPlater

--- js/streams.js
+++ js/streams.js
@@ -1,15 +1,15 @@
-const fileSystem = require('fs');
+import { existsSync, writeFile, createReadStream, createWriteStream } from 'node:fs';
 
-if(!fileSystem.existsSync('./docs/blog3.txt')) {
+if(!existsSync('./docs/blog3.txt')) {
     console.log('The file is being created...')
-    fileSystem.writeFile('./docs/blog3.txt', '//This file will be used alongside while studying about streams and buffer !', () => {
+    writeFile('./docs/blog3.txt', '//This file will be used alongside while studying about streams and buffer !', () => {
         console.log('File created Successfully...');
     })
 } else {
     console.log('File blog3.txt arleady exist feel free to use it !!!')
 };
 
-const readStream = fileSystem.createReadStream("./docs/blog4.txt");
-const writeStream = fileSystem.createWriteStream("./docs/blog11.txt");
+const readStream = createReadStream("./docs/blog4.txt");
+const writeStream = createWriteStream("./docs/blog11.txt");
 
 readStream.pipe(writeStream);
