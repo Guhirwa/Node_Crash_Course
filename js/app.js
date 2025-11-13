803e3aeb175bd1591122d2111f30885f69c06816
Guhirwa <christianguhirwa74@gmail.com>
2025-11-13 17:12:51 +0200

    build(parentDir): Install React app and Tailwindcss framework
    install tailwindcss for creating some design and styling easily using utility first classes

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+// Express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// in case views folder have another name like `myviews`
+// app.set('views', 'myviews');
+
+// listen for request
+app.listen(3000);
+
+app.get('/', (request, responce) => {
+    responce.render('index', { title: "Home" });
+});
+
+app.get('/about', (request, responce) => {
+    responce.render('about', { title: "Home" });
+});
+
+app.get('/blogs/create', (request, responce) => {
+    responce.render('create');
+});
+
+// redirects
+app.get('/about-me', (request, responce) => {
+    responce.redirect('/about');
+});
+
+app.get('/about-us', (request,responce) => {
+    responce.redirect('/about');
+});
+
+// 404 page
+app.use((request, responce) => {
+    responce.status(404).render('404');
+});
